8aaf38c4c39dfd0feba6d6fe4466d7d7d0651972
mlpdleal <manoellealdeveloper@gmail.com>
2022-04-27 13:34:36 -0300

    Completed 2 of #100DaysOfSwiftUI challange

---
+++ day2/Checkpoint_1.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+// Challange 1
+
+let celsiusTemperature = 27.0
+let fahrenheitTemperature = Double(((celsiusTemperature * 9) / 5) + 32)
+print("The temperature in Celsius is \(celsiusTemperature)° and in Fahrenheit is \(fahrenheitTemperature)°")
+// Challange 2

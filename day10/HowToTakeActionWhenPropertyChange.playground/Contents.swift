f432bfb7afd066932e154be319e14df87a202790
mlpdleal <manoellealdeveloper@gmail.com>
2022-05-05 23:40:13 -0300

    Completed day 10 of 100 days of SwiftUI

---
+++ day10/HowToTakeActionWhenPropertyChange.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Cocoa
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score now is \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value wil be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Jôsi")
+app.contacts.append("Manoel")

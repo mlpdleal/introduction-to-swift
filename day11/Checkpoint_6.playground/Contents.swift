73573ceb1c37770448f99037f9500d1cbb70f24b
mlpdleal <manoellealdeveloper@gmail.com>
2022-05-06 20:36:38 -0300

    checkpoint

--- day11/Checkpoint_6.playground/Contents.swift
+++ day11/Checkpoint_6.playground/Contents.swift
@@ -1,10 +1,15 @@
 import Cocoa
 
 struct Car {
-    let model: String {
-    let numberOfSeats: Int
+    private let model: String
+    private let numberOfSeats: Int
     private var currentGear: Int = 1
     
+    init(model: String, numberOfSeats: Int){
+        self.model = model
+        self.numberOfSeats = numberOfSeats
+    }
+        
     mutating func changeGears(up: Bool){
         if up {
             self.currentGear += 1
@@ -13,3 +18,10 @@ struct Car {
         }
     }
 }
+
+
+let audi = Car(model: "Audi a3", numberOfSeats: 5)
+
+
+
+
